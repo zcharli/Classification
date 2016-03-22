3326ccccf9f544ea725a3281152b0dd7256b4e5a
Charlie Li <gogogeorge123@yahoo.ca>
2016-03-22 17:47:32 -0400

    Implemented 10 fold classification by separating classes out first. Leads to a poor heart disease classification :(

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ import sys
 # np.seterr(invalid='ignore',divide='ignore')
 
 # Enums
-LINEAR = 0
-OPTIMAL_BAYES = 1
-NAIVE_BAYES = 2
-
+LINEAR, OPTIMAL_BAYES, NAIVE_BAYES = "LINEAR", "OPTIMAL BAYES", "NAIVE BAYES"
 # Useful settings keys
-COVARIANCE = "train_classes_cov_dict"
-MEAN = "train_classes_mean_dict"
-CLASSES = "train_classes_dict"
+COVARIANCE, MEAN, CLASS_INDEX, TITLE\
+    = "train_classes_cov", "train_classes_mean","classIndex","title"
+TEN_FOLD, T_CLASS, TEST_BATCH, TRAIN, TEST = "10_fold_batches", "10_fold_classes", "test_data", 0, 1
 
 IRIS_SETTING = {"usecols": (0,1,2,3,4), "classIndex":4,
                "data": "./data/iris.csv",
@@ -22,88 +19,88 @@ IRIS_SETTING = {"usecols": (0,1,2,3,4), "classIndex":4,
 WINE_SETTING = {"usecols": (0,1,2,3,4,5,6,7,8,9,10,11,12,13),"classIndex":0,
                "data": "./data/wine.csv",
                "title":"WINE"}
-HEART_SETTING = {"usecols": (0,2,3,4,5,6,7,8,9,10,11,12,13),"classIndex":12,
+HEART_SETTING = {"usecols": (0,2,3,4,6,7,9,10,11,12,13),"classIndex":10,
                "data": "./data/heartDisease.csv",
                "title":"HEARTDISEASE"}
 
 
 def main(s, strat):
-    trainTestBatch = splitTenFold(loadCSV(s["data"], s), s)
-    s["10_fold_batches"] = trainTestBatch
+    splitTenFold(loadCSV(s["data"], s), s)
     train(s, strat)
+    test(s, strat)
 
 def loadCSV(path, s):
     return np.genfromtxt(path, dtype=None, usecols=s["usecols"], names=True, delimiter=',')
 
 def processTuple(t,s):
-    if t[s['classIndex']].dtype.type is np.string_:
-        l = [t[x] for x in xrange(s['classIndex']+1)]# + t[s['classIndex']]
+    if t[s[CLASS_INDEX]].dtype.type is np.string_:
+        l = [t[x] for x in xrange(s['classIndex']+1)]
         return np.asarray(l, dtype=object)
     return np.asarray(np.asarray(t,dtype=object).item(0))
 
 def splitTenFold(data, s):
-    splitIdx = skc.KFold(len(data), n_folds=10) #, shuffle=True)
-    tenFold = []
-    for train, test in splitIdx:
-        tenFold.append(([processTuple(data[j], s) for j in train],
-                        [processTuple(data[j], s) for j in test]))
-    return tenFold
+    classes, s[T_CLASS] = getClasses(data, s), dict()
+    for key in classes.keys():
+        splitIdx = skc.KFold(len(classes[key]), n_folds=10, shuffle=True)
+        s[T_CLASS][key] = dict()
+        s[T_CLASS][key][TEST_BATCH] = []
+        for train, test in splitIdx:
+            s[T_CLASS][key][TEST_BATCH].append(([classes[key][j] for j in train],
+                                           [classes[key][j] for j in test]))
 
-def getMeanByClasses(sampleClassDict):
-    classMeans = dict()
-    for key in sampleClassDict.keys():
-        classMeans[key] = np.mean(sampleClassDict[key], axis=0)
-    return classMeans
-
-def getCovarianceMatrix(sampleClassDict, strat):
-    classCov = dict()
+def getCovarianceMatrix(dataset, strat):
     if strat == LINEAR:
         return 0
     else:
-        for key in sampleClassDict.keys():
-            classCov[key] = np.cov(np.array(sampleClassDict[key]).T)
-            if strat == NAIVE_BAYES:
-                classCov[key] = np.diag(np.diag(classCov[key]))
-    return classCov
+        return np.cov(np.array(dataset).T) if strat == OPTIMAL_BAYES else np.diag(np.diag(np.cov(np.array(dataset).T)))
 
-
-def getClasses(tupleArray, i):
-    classes = dict()
+def getClasses(tupleArray, s):
+    classes, i = dict(), s[CLASS_INDEX]
     if len(tupleArray) == 0: raise ValueError("No classes to get.")
     for sample in tupleArray:
-        sampleClass = sample[i]
+        sampleClass, sampleFeatures = sample[i], processTuple(sample,s)
         if sampleClass in classes:
-            classes[sampleClass].append(np.delete(sample, i))
+            classes[sampleClass].append(np.delete(sampleFeatures, i))
         else:
-            classes[sampleClass] = [np.delete(sample, i)]
+            classes[sampleClass] = [np.delete(sampleFeatures, i)]
     return classes
 
-def train(s, strat):
-    avgCorrect = 0
-    for batches in s["10_fold_batches"]:
-        trainSample, testSample = batches[0], batches[1]
-        s[CLASSES] = getClasses(trainSample, s["classIndex"])
-        s[MEAN] = getMeanByClasses(s[CLASSES])
-        s[COVARIANCE] = getCovarianceMatrix(s[CLASSES], strat)
-        batchCorrect,batchTotal = 0, len(trainSample)
-        for x in trainSample:
-            trueClass = x[s['classIndex']]
-            classifiedClass = classify(s, np.delete(x,s['classIndex']), strat)
-            batchCorrect = batchCorrect + 1 if trueClass == classifiedClass else batchCorrect
-        avgCorrect += (batchCorrect/batchTotal)
-    print "10-Fold accuracy for %s dataset is %.2f" % (s["title"], avgCorrect/10)
+def test(s, strat):
+    totalCorrectness = 0
+    for key in s[T_CLASS].keys():
+        classCorrectness = 0
+        for batchNumber in xrange(len(s[T_CLASS][key])):
+            testSample = s[T_CLASS][key][TEST_BATCH][batchNumber][TEST]
+            classCorrect, classTotal = 0, len(testSample)
+            for x in testSample:
+                classifiedClass = classify(s, x, strat, batchNumber)
+                classCorrect = classCorrect + 1 if key == classifiedClass else classCorrect
+            classCorrectness += (classCorrect/classTotal)
+        print "%s class %s for %s accuracy %0.2f%%" % (s[TITLE], str(key), strat,
+                                                       (classCorrectness/len(s[T_CLASS].keys()))*100)
+        totalCorrectness += classCorrectness
+    print "10-Fold accuracy using %s for %s dataset is %.2f%%\n" % (strat, s[TITLE],
+                                                                100*(totalCorrectness/(len(s[T_CLASS].keys())**2)))
 
-def classify(s, x, strat):
-    classes = s[CLASSES].keys()
+def train(s, strat):
+    for key in s[T_CLASS].keys():
+        s[T_CLASS][key][MEAN], s[T_CLASS][key][COVARIANCE] = [], []
+        for i in xrange(len(s[T_CLASS][key][TEST_BATCH])):
+            trainSample = s[T_CLASS][key][TEST_BATCH][i][TRAIN]
+            s[T_CLASS][key][MEAN].append(np.mean(trainSample, axis=0))
+            s[T_CLASS][key][COVARIANCE].append(getCovarianceMatrix(trainSample, strat))
+
+def classify(s, x, strat, b):
+    classes = s[T_CLASS].keys()
     if len(classes) < 2: raise ValueError("Cannot classify with only one class")
     decKey = classes[0]
     for idx in range(1, len(classes)):
         if strat == LINEAR:
             c = 0
         else:
-            c = getLn(s[COVARIANCE][classes[idx]]) - getLn(s[COVARIANCE][decKey]) + \
-                mahalanobisDistance(x, s[MEAN][classes[idx]], s[COVARIANCE][classes[idx]]) - \
-                mahalanobisDistance(x, s[MEAN][decKey], s[COVARIANCE][decKey])
+            c = getLn(s[T_CLASS][classes[idx]][COVARIANCE][b]) - getLn(s[T_CLASS][decKey][COVARIANCE][b]) + \
+                mahalanobisDistance(x, s[T_CLASS][classes[idx]][MEAN][b], s[T_CLASS][classes[idx]][COVARIANCE][b]) - \
+                mahalanobisDistance(x, s[T_CLASS][decKey][MEAN][b], s[T_CLASS][decKey][COVARIANCE][b])
         if c < 0:
             decKey = classes[idx]
     return decKey
@@ -113,7 +110,7 @@ def getLn(covarianceMatrix):
         return np.log(np.linalg.det(covarianceMatrix))
     else:
         d = np.linalg.det(covarianceMatrix)
-        if d == 0: return 0
+        if d == 0: return 0 # if d <= 0: return 0
         return np.log(d) if d != 0 else d
 
 def mahalanobisDistance(x, m, c):
@@ -124,12 +121,12 @@ def mahalanobisDistance(x, m, c):
     return reduce(np.dot, [np.array(x-m).T, l, x-m])
 
 if __name__ == "__main__":
-   main(WINE_SETTING, OPTIMAL_BAYES)
-   main(IRIS_SETTING, OPTIMAL_BAYES)
+   #main(WINE_SETTING, OPTIMAL_BAYES)
+   #main(IRIS_SETTING, OPTIMAL_BAYES)
    main(HEART_SETTING, OPTIMAL_BAYES)
-   main(HEART_SETTING, NAIVE_BAYES)
-   main(IRIS_SETTING, NAIVE_BAYES)
-   main(WINE_SETTING, NAIVE_BAYES)
+   #main(HEART_SETTING, NAIVE_BAYES)
+   #main(IRIS_SETTING, NAIVE_BAYES)
+   #main(WINE_SETTING, NAIVE_BAYES)
 
 # Not needed
 # def manualCov(vectorList, mean):
@@ -140,3 +137,18 @@ if __name__ == "__main__":
 #         m1 = np.multiply(x1,x2)
 #         c += m1
 #     return (1/(len(vectorList)-1)) * c
+# def getCovarianceMatrix(sampleClassDict, strat):
+#     classCov = dict()
+#     if strat == LINEAR:
+#         return 0
+#     else:
+#         for key in sampleClassDict.keys():
+#             classCov[key] = np.cov(np.array(sampleClassDict[key]).T)
+#             if strat == NAIVE_BAYES:
+#                 classCov[key] = np.diag(np.diag(classCov[key]))
+#     return classCov
+# def getMeanByClasses(sampleClassDict):
+#     classMeans = dict()
+#     for key in sampleClassDict.keys():
+#         classMeans[key] = np.mean(sampleClassDict[key], axis=0)
+#     return classMeans
